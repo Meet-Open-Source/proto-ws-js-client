5766de627c8f2b557a16a580f3964b08167f5061
Mario Kapusta <mario.kapusta@ubisoft.com>
2021-01-15 22:38:20 +0100

    Fix typo in todos.

--- src/serviceImpl.js
+++ src/serviceImpl.js
@@ -1,19 +1,19 @@
 import SockJS from 'sockjs-client';
 
-const callbacks = {}; //TODO create a timer for cleaning the callbacks (configurable timout - default 30sec)
+const callbacks = {}; //todo create a timer for cleaning the callbacks (configurable timout - default 30sec)
 
 let index = 0;
 
-const sock = new SockJS('TODO');
+const sock = new SockJS('todo');
 
 sock.onopen = function() {
-  //todo communcate to server the protocol type (binary or string)
+  //todo communicate to server the protocol type (binary or string)
   console.log('open');
 };
 
 sock.onmessage = function(event) {
 
-  if(event.data.error != null) { //TODO data need to be converted to proto msgs from json string
+  if(event.data.error != null) { //todo data need to be converted to proto msgs from json string
     callbacks[event.data.index](event.data.error, null)
   } else {
     callbacks[event.data.index](null, event.data)
@@ -27,7 +27,7 @@ sock.onclose = function() {
 };
 
 function serviceInterceptor(serviceName, requestData, callback) {
-  //TODO create Request wrapper proto message and inject index and convert to JSON
+  //todo create Request wrapper proto message and inject index and convert to JSON
 
   try {
     index++;
@@ -37,7 +37,7 @@ function serviceInterceptor(serviceName, requestData, callback) {
     callbacks[index] = callback;
   } catch (error) {
     console.error(error);
-    throw error; //TODO create custom exception
+    throw error; //todo create custom exception
   }
 }
 
